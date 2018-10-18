4189874bf487a41d0a81db97ff373f27625ccb07
jared50000 <jared50000@gmail.com>
2018-10-17 22:17:47 -0500

    Set up basic side scrolling demo

---
+++ VetLife/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+    public float smoothTime = 0.3F;
+	public float posY;
+	public float minX;
+	public float maxX;
+	
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, posY, -10));
+
+        // Smoothly move the camera towards that target position
+        Vector3 desiredPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		transform.position = new Vector3(Mathf.Clamp(desiredPosition.x, minX, maxX), desiredPosition.y, desiredPosition.z);
+    }
+}
---
+++ VetLife/Assets/Characters/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed;
+	public bool shouldFaceLeft;
+	public float maxX;
+	public float minX;
+	
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		
+		//move with the horizontal input
+		if((horizontal > 0 && transform.position[0] < maxX) || (horizontal < 0 && transform.position[0] > minX))
+		{
+			transform.Translate(horizontal * Time.deltaTime * speed, 0f, 0f);
+		}
+		
+		//face in the direction last moved
+		if(horizontal > 0)
+		{
+			shouldFaceLeft = false;
+		}
+		else if(horizontal < 0)
+		{
+			shouldFaceLeft = true;
+		}
+		spriteRenderer.flipX = shouldFaceLeft;
+	}
+}
