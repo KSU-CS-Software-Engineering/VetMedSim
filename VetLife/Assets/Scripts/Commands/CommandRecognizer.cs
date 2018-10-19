ca1e5ab1b08d9a510519f5870877b95f3e9d1c0b
Matej Shanel <shanemat@ksu.edu>
2018-10-18 23:39:15 -0500

    Add user command recognition

---
+++ VetLife/Assets/Scripts/Commands/CommandRecognizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Interaction.Prototype;
+using UnityEngine;
+
+namespace Assets.Scripts.Commands
+{
+	/// <summary>
+	/// Static object providing means to distinguish user commands
+	/// </summary>
+	public static class CommandRecognizer
+	{
+		#region Functions
+
+		/// <summary>
+		/// Determines type of user input command
+		/// </summary>
+		/// <param name="interactives">List of interactive objects in proximity</param>
+		/// <param name="mainTouch">Current main touch gesture</param>
+		/// <returns>Type of user input command</returns>
+		public static CommandType Recognize( List<IInteractive> interactives, Touch? mainTouch )
+		{
+			if( !mainTouch.HasValue )
+			{
+				return CommandType.NONE;
+			}
+
+			var touchPoint = Camera.main.ScreenToWorldPoint( mainTouch.Value.position );
+			if( interactives.Exists( interactive => interactive.IsTargeted( touchPoint ) ) )
+			{
+				return CommandType.INTERACTION;
+			}
+			else
+			{
+				return CommandType.MOVEMENT;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ VetLife/Assets/Scripts/Commands/CommandType.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Assets.Scripts.Commands
+{
+	/// <summary>
+	/// Represents different user commands
+	/// </summary>
+	public enum CommandType
+	{
+		MOVEMENT,
+		INTERACTION,
+		NONE
+	}
+}
