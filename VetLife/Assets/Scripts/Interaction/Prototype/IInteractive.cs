9d2e33de0394b22d35f14073b31ccd040f02993d
Matej Shanel <shanemat@ksu.edu>
2018-10-18 23:39:57 -0500

    Add interactivity interfaces

---
+++ VetLife/Assets/Scripts/Interaction/Prototype/IInteractive.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interaction.Prototype
+{
+	/// <summary>
+	/// Objects, which can be interacted with
+	/// </summary>
+	public interface IInteractive
+	{
+		/// <summary>
+		/// Starts interaction attempt
+		/// </summary>
+		void StartInteraction();
+
+		/// <summary>
+		/// Aborts any attempt of interaction
+		/// </summary>
+		void AbortInteraction();
+
+		/// <summary>
+		/// Checks whether given position is within trigger bounds of this object
+		/// </summary>
+		/// <param name="position">Position to be checked</param>
+		/// <returns>True in case position selects bounds of this object, false otherwise</returns>
+		bool IsTargeted( Vector2 position );
+	}
+}
---
+++ VetLife/Assets/Scripts/Interaction/Prototype/IInteractor.cs
@@ -0,0 +1,20 @@
+﻿namespace Assets.Scripts.Interaction.Prototype
+{
+	/// <summary>
+	/// Interface providing methods of objects able of interacting
+	/// </summary>
+	public interface IInteractor
+	{
+		/// <summary>
+		/// Notify's this object, that given interactive object is in their range
+		/// </summary>
+		/// <param name="interactive"></param>
+		void AddInteractive( IInteractive interactive );
+
+		/// <summary>
+		/// Notify's this object, that given interactive object is no longer in range
+		/// </summary>
+		/// <param name="interactive"></param>
+		void RemoveInteractive( IInteractive interactive );
+	}
+}
