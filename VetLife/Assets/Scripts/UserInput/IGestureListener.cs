42f56efe9301d92ab13b6ec0150957f3cda995e8
Matej Shanel <shanemat@ksu.edu>
2018-11-15 10:30:53 -0600

    Add gesture listener interface definition

---
+++ VetLife/Assets/Scripts/UserInput/IGestureListener.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Assets.Scripts.UserInput
+{
+	/// <summary>
+	/// Allows object to receive <see cref="Gesture"/> notification from <see cref="GestureHandler"/>
+	/// </summary>
+	public interface IGestureListener
+	{
+		/// <summary>
+		/// Defines behavior on given <see cref="Gesture"/> being registered
+		/// </summary>
+		/// <param name="gesture"><see cref="Gesture"/> to react to</param>
+		void OnGestureStart( Gesture gesture );
+
+		/// <summary>
+		/// Defines behavior on given <see cref="Gesture"/> being finished
+		/// </summary>
+		/// <param name="gesture"><see cref="Gesture"/> to react to its end</param>
+		void OnGestureEnd( Gesture gesture );
+	}
+}
