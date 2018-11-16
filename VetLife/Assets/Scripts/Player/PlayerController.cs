dc9f34e1bf4d6a4afef866bb9a95b1075bed9983
Matej Shanel <shanemat@ksu.edu>
2018-11-15 22:50:44 -0600

    Add player controller script

---
+++ VetLife/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Scripts.UserInput;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+	/// <summary>
+	/// Defines the behavior of the player sprite
+	/// </summary>
+	[RequireComponent( typeof( Animator ) )]
+	public class PlayerController : MonoBehaviour
+	{
+		#region Constants
+
+		/// <summary>
+		/// Name of the animation trigger for movement
+		/// </summary>
+		internal string MOVE_ANIMATION_TRIGGER = "Moves";
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Object handling gesture recognition
+		/// </summary>
+		public GestureHandler GestureHandler;
+
+		/// <summary>
+		/// Speed by which the player character moves
+		/// </summary>
+		public float Speed;
+
+		/// <summary>
+		/// Animator component of player object
+		/// </summary>
+		internal Animator Animator { get; private set; }
+
+		/// <summary>
+		/// Current position of the player
+		/// </summary>
+		internal Vector2 Position => gameObject.transform.position;
+
+		#endregion
+
+		#region Overrides
+
+		private void Awake()
+		{
+			Animator = GetComponent<Animator>();
+		}
+
+		#endregion
+	}
+}
