d8519f218706764b5dfeed18df30af20e68c6334
Matej Shanel <shanemat@ksu.edu>
2018-12-03 16:05:48 -0600

    Add interaction option implementation

---
+++ VetLife/Assets/Scripts/Interaction/OptionButton.cs
@@ -0,0 +1,71 @@
+﻿using Assets.Scripts.Interaction.Options;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Interaction
+{
+	/// <summary>
+	/// Represents the button in option dialog holding one of the options
+	/// </summary>
+	public class OptionButton : MonoBehaviour
+	{
+		#region Fields
+
+		/// <summary>
+		/// Currently attached interaction option
+		/// </summary>
+		private InteractionOption _option;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Button controlling component of this button
+		/// </summary>
+		public Button ButtonComponent;
+
+		/// <summary>
+		/// Text component attached to this button
+		/// </summary>
+		public Text TextComponent;
+
+		#endregion
+
+		#region Overrides
+
+		void Start()
+		{
+			if( _option != null )
+			{
+				ButtonComponent.onClick.AddListener( HandleClick );
+			}
+		}
+
+		#endregion
+
+		#region Functions
+
+		/// <summary>
+		/// Handles the situation, when this button gets clicked
+		/// </summary>
+		public void HandleClick()
+		{
+			_option.OnSelected();
+		}
+
+		/// <summary>
+		/// Populates this button by given interaction option
+		/// </summary>
+		/// <param name="option"><see cref="InteractionOption"/> which data should populate this button</param>
+		public void PopulateBy( InteractionOption option )
+		{
+			_option = option;
+			TextComponent.text = _option.Name;
+
+			Debug.Log( "Populated!" );
+		}
+
+		#endregion
+	}
+}
---
+++ VetLife/Assets/Scripts/Interaction/Options/InteractionOption.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Interaction.Options
+{
+	/// <summary>
+	/// Holds common functionality and interface description for interaction options
+	/// </summary>
+	public abstract class InteractionOption
+	{
+		#region Fields
+
+		/// <summary>
+		/// Reference to the dialog this interaction option is currently bound to
+		/// </summary>
+		private OptionDialog _dialog;
+
+		/// <summary>
+		/// Function invoked when this option is selected
+		/// </summary>
+		private readonly Action _callback;
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Name of the option
+		/// </summary>
+		public string Name { get; private set; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Constructs base interaction option
+		/// </summary>
+		/// <param name="name">Name of the option</param>
+		/// <param name="callbackFunction">Function invoked when this option is selected</param>
+		protected InteractionOption( string name, Action callbackFunction )
+		{
+			Name = name;
+			_callback = callbackFunction;
+		}
+
+		#endregion
+
+		#region Functions
+
+		/// <summary>
+		/// Creates backlink to the given <see cref="OptionDialog"/>
+		/// </summary>
+		/// <param name="dialog">Dialog to attach this option to</param>
+		public void AttachToDialog( OptionDialog dialog )
+		{
+			_dialog = dialog;
+		}
+
+		/// <summary>
+		/// Removes backlink to any linked <see cref="OptionDialog"/>
+		/// </summary>
+		public void DetachFromDialog()
+		{
+			_dialog = null;
+		}
+
+		/// <summary>
+		/// Callback function which gets called on this option being selected
+		/// </summary>
+		public void OnSelected()
+		{
+			_callback.Invoke();
+		}
+
+		/// <summary>
+		/// Dismisses the parent dialog and clears its options
+		/// </summary>
+		protected void DismissDialog()
+		{
+			_dialog.Dismiss();
+			_dialog.ClearOptions();
+		}
+
+		#endregion
+	}
+}
