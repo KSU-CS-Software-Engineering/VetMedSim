489513fdc3e28333f7bc21ce7a59c38de814198e
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-02-19 14:11:57 -0600

    Added IInteractable Objects interface.

---
+++ VetLife/Assets/Scripts/Interaction/Sprite/IInteractiveObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Interaction.Options.Sprite
+{
+    public interface IInteractiveObject
+    {
+
+    }
+}
---
+++ VetLife/Assets/Scripts/Interaction/UI Interaction/ShowCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCanvas : MonoBehaviour
+{
+    public GameObject CanvasObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ActivateUI()
+    {
+        CanvasObject.GetComponent<Canvas>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
