21769e347ddd1d1fbecefcdeed1e300bf1457245
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-03-19 23:42:05 -0500

    Added interaction to cabinet2`
    Added click popup interaction and tap popup interaction to the labratory cabinet2

---
+++ VetLife/Assets/Scripts/Player/CabinetInteraction.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.UserInput;
+
+namespace Assets.Scripts.Player
+{
+    public class CabinetInteraction : MonoBehaviour, IGestureListener
+    {
+
+        public Canvas canvas;
+
+        // Use this for initialization
+        void Start()
+        {
+            canvas.enabled = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            // Move this object to the position clicked by the mouse.
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+                if (hit.collider != null)
+                {
+                    Debug.Log("I'm hitting " + hit.collider.name);
+                    if (hit.collider.name.Equals("cabinet2"))
+                    {
+                        canvas.enabled = !canvas.enabled;
+                    }
+                }
+            }
+        }
+
+        public void OnGestureStart(Gesture gesture)
+        {
+            switch (gesture.Type)
+            {
+                case GestureType.Tap:
+                    var origin = ((Tap)gesture).Origin;
+                    var destination = Camera.main.ScreenToWorldPoint(origin);
+                    RaycastHit2D hit = Physics2D.Raycast(destination, Vector2.zero);
+                    if (hit.collider != null)
+                    {
+                        Debug.Log("I'm hitting " + hit.collider.name);
+                        if (hit.collider.name.Equals("cabinet2"))
+                        {
+                            canvas.enabled = !canvas.enabled;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public void OnGestureEnd(Gesture gesture)
+        {
+            // intentionally left blank
+        }
+    }
+}
