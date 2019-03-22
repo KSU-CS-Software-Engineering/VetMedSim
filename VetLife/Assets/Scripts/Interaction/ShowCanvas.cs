100c29815c93ab34fd8e6ba26b126e9904c9f5b6
ashley <asmorton@ksu.edu>
2019-03-22 15:35:49 -0600

    implement menu

--- VetLife/Assets/Scripts/Interaction/ShowCanvas.cs
+++ VetLife/Assets/Scripts/Interaction/ShowCanvas.cs
@@ -1,19 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Assets.Scripts.UserInput;
 
 public class ShowCanvas : MonoBehaviour
 {
     public GameObject CanvasObject;
-
+    public GestureType _gestureType = new GestureType();
     // Start is called before the first frame update
     void Start()
     {
-        CanvasObject.GetComponent<Canvas>().enabled = false;
+       CanvasObject.GetComponent<Canvas>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        CanvasObject.GetComponent<Canvas>().enabled = true;
     }
 
     public void ActivateUI()
     {
+        Debug.Log("Activate UI");
         CanvasObject.GetComponent<Canvas>().enabled = true;
         CanvasObject.SetActive(true);
     }
@@ -27,6 +32,11 @@ public class ShowCanvas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Clicked");
+            CanvasObject.GetComponent<Canvas>().enabled = true;
+        }
         
     }
 }
