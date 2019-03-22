b763fb086e1f6c6b2ae1ae702f9cc924019724e1
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-03-21 23:22:47 -0500

    Interaction menu implementation (#10)
    * Added interaction to cabinet2`
    
    Added click popup interaction and tap popup interaction to the labratory cabinet2
    
    * Creation of a template for interaction
    
    * updated usings to alphabetical order
    
    * Renamed destination to gestureLocation
    
    * Updated code to fit style guide
    
    * Update CabinetInteraction.cs
    
    * Update CabinetInteraction.cs
    
    * Fixed _canvas naming bug

---
+++ VetLife/Assets/Scripts/Player/CabinetInteraction.cs
@@ -0,0 +1,72 @@
+﻿using Assets.Scripts.UserInput;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class CabinetInteraction : MonoBehaviour, IGestureListener
+    {
+        #region Fields
+
+        /// <summary>
+        /// Canvas to used displayed when the click/tap event is triggered
+        /// </summary>
+        public Canvas _canvas;
+        
+        #endregion
+
+        #region Overrides
+        void Start()
+        {
+            _canvas.enabled = false;
+        }
+
+        
+        void Update()
+        {
+
+            if( Input.GetMouseButtonDown( 0 ) )
+            {
+                Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+                if( hit.collider != null )
+                {
+                    Debug.Log( "I'm hitting " + hit.collider.name );
+                    if( hit.collider.name.Equals( "cabinet2" ) )
+                    {
+                        _canvas.enabled = !_canvas.enabled;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region IGestureListener
+        public void OnGestureStart( Gesture gesture )
+        {
+            switch( gesture.Type )
+            {
+                case GestureType.Tap:
+                    var origin = ( (Tap) gesture ).Origin;
+                    var gestureLocation = Camera.main.ScreenToWorldPoint( origin );
+                    RaycastHit2D hit = Physics2D.Raycast( gestureLocation, Vector2.zero );
+                    if( hit.collider != null )
+                    {
+                        Debug.Log( "I'm hitting " + hit.collider.name );
+                        if( hit.collider.name.Equals( "cabinet2" ) )
+                        {
+                            _canvas.enabled = !_canvas.enabled;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public void OnGestureEnd( Gesture gesture )
+        {
+            // intentionally left blank
+        }
+        #endregion
+    }
+}
