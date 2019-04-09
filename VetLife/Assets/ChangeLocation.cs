29fa0e0a53f17a0f79222a71e01fd97f5f359d81
Michael Yangmi <myangmi@ksu.edu>
2019-04-09 01:15:51 -0500

    A* somewhat implemented
    test

---
+++ VetLife/Assets/ChangeLocation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLocation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (var i = 0; i < Input.touchCount; i++)
+        {
+
+
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+
+
+                transform.position = new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, transform.position.z);
+
+            }
+        }
+    }
+}
