b057d6f0e3d85eab3aac4cffc50a8a4853d2dcb7
jared.vincent <jared50000@gmail.com>
2019-04-13 10:47:04 -0500

    Add scripts for toggling GameObjects and triggering events based on gestures

---
+++ VetLife/Assets/Scripts/Object/ToggleEnabled.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Object
+{
+    public class ToggleEnabled : MonoBehaviour
+    {
+        #region Functions
+        public void EnableObject()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void DisableObject()
+        {
+            gameObject.SetActive(false);
+        }
+
+        #endregion
+    }
+}
