1ab5960ccc5d880056228474bf0b784c9b36c068
jared.vincent <jared50000@gmail.com>
2019-04-13 10:59:50 -0500

    Add missing function documentation

--- VetLife/Assets/Scripts/Object/ToggleEnabled.cs
+++ VetLife/Assets/Scripts/Object/ToggleEnabled.cs
@@ -7,11 +7,17 @@ namespace Assets.Scripts.Object
     public class ToggleEnabled : MonoBehaviour
     {
         #region Functions
+        /// <summary>
+        /// Activates attached object
+        /// </summary>
         public void EnableObject()
         {
             gameObject.SetActive(true);
         }
 
+        /// <summary>
+        /// Deactivates attached object
+        /// </summary>
         public void DisableObject()
         {
             gameObject.SetActive(false);
