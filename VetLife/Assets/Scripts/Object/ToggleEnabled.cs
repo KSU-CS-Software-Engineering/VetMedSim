acf14f05d776122daf7edd039e60c878379d75f7
Jared Vincent <jared50000@gmail.com>
2019-04-16 16:05:54 -0500

    Close panels (#13)
    * Add scripts for toggling GameObjects and triggering events based on gestures
    
    * Add missing function documentation
    
    * Fix spacing mistake

---
+++ VetLife/Assets/Scripts/Object/ToggleEnabled.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Object
+{
+    public class ToggleEnabled : MonoBehaviour
+    {
+        #region Functions
+        /// <summary>
+        /// Activates attached object
+        /// </summary>
+        public void EnableObject()
+        {
+            gameObject.SetActive( true );
+        }
+
+        /// <summary>
+        /// Deactivates attached object
+        /// </summary>
+        public void DisableObject()
+        {
+            gameObject.SetActive( false );
+        }
+
+        #endregion
+    }
+}
---
+++ VetLife/Assets/Scripts/UserInput/OnGesture.cs
@@ -0,0 +1,55 @@
+﻿using Assets.Scripts.UserInput;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UserInput
+{
+    /// <summary>
+    /// Class that listens for gestures and invokes the attached GameObject's onclick() event when appropriate
+    /// </summary>
+    public class OnGesture : MonoBehaviour, IGestureListener
+    {
+        #region Properties
+        /// <summary>
+        /// Object handling gesture recognition
+        /// </summary>
+        public GestureHandler GestureHandler;
+
+        #endregion
+
+        #region Overrides
+        private void Awake()
+        {
+            GestureHandler.RegisterListener( this );
+        }
+
+        #endregion
+
+        #region IGestureListener
+        public void OnGestureStart( Gesture gesture )
+        {
+            switch ( gesture.Type )
+            {
+                case GestureType.Tap:
+                    Vector2 origin = ( (Tap)gesture ).Origin;
+                    Vector3 gestureLocation = Camera.main.ScreenToWorldPoint( origin );
+                    RaycastHit2D hit = Physics2D.Raycast( gestureLocation, Vector2.zero );
+                    if ( hit.transform.gameObject == gameObject )
+                    {
+                        gameObject.GetComponent<Button>().onClick.Invoke();
+                    }
+                    break;
+            }
+        }
+
+        public void OnGestureEnd( Gesture gesture )
+        {
+            //intentionally left blank
+        }
+
+        #endregion
+    }
+
+}
