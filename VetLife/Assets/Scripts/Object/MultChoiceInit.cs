751fa2d35a258ed26cfda212affe8f2d64d8dd1d
Jared Vincent <jared50000@gmail.com>
2019-05-07 09:41:43 -0500

    Create multiple-choice toenail clipping UI (#15)

---
+++ VetLife/Assets/Scripts/Object/MultChoiceInit.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Object
+{
+    /// <summary>
+    /// Initialization and reinitialization of a multiple-choice type question
+    /// </summary>
+    public class MultChoiceInit : MonoBehaviour
+    {
+        #region Properties
+        public GameObject answer;
+        public GameObject options;
+        public GameObject results;
+        #endregion
+
+        #region Overrides
+
+        void OnEnable()
+        {
+            ToggleEnabled answerToggle = answer.GetComponent<ToggleEnabled>();
+            ToggleChildrenEnabled resultsToggle = results.GetComponent<ToggleChildrenEnabled>();
+
+            if( answerToggle != null )
+            {
+                answerToggle.DisableObject();
+            }
+
+            if( resultsToggle != null )
+            {
+                resultsToggle.DisableChildren();
+            }
+            
+            foreach( Transform optionTransform in options.transform )
+            {
+                Button option = optionTransform.GetComponent<Button>();
+                if( option != null )
+                {
+                    option.interactable = true;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ VetLife/Assets/Scripts/Object/ToggleChildrenEnabled.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Object
+{
+    /// <summary>
+    /// Activates or deactivates all objects in children
+    /// </summary>
+    public class ToggleChildrenEnabled : MonoBehaviour
+    {
+        #region Functions
+        /// <summary>
+        /// Activates attached object's children (recursively if applicable)
+        /// </summary>
+        public void EnableChildren()
+        {
+            foreach( Transform child in transform )
+            {
+                GameObject childObject = child.gameObject;
+                childObject.SetActive( true );
+                ToggleChildrenEnabled childToggle = childObject.GetComponent<ToggleChildrenEnabled>();
+                if( childToggle != null )
+                {
+                    childToggle.EnableChildren();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deactivates attached object's children (recursively if applicable)
+        /// </summary>
+        public void DisableChildren()
+        {
+            foreach( Transform child in transform )
+            {
+                GameObject childObject = child.gameObject;
+                childObject.SetActive( false );
+                ToggleChildrenEnabled childToggle = childObject.GetComponent<ToggleChildrenEnabled>();
+                if( childToggle != null )
+                {
+                    childToggle.DisableChildren();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
